d9e9b9648b15fe56885bbf81a45d365252aa0b3b
jeasson <jeassonsuarez@outlook.es>
2022-10-02 18:51:05 -0500

    avance 10

--- src/Routes/index.routes.js
+++ src/Routes/index.routes.js
@@ -7,7 +7,9 @@ const router = Router();
 router.get('/sesion', async(req, res)=>{
     const usuarios = await db.query('SELECT * FROM users')
     //console.log(req);
+    console.log(usuarios);
     res.send(usuarios)
+
 })
 
 export default router;
